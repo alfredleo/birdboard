04571ecf155f47a45d247f0939444987f93d98fc
Alfred <alfredleo@gmail.com>
2021-07-05 15:44:42 +0500

    refactor: make test run fine with getting all projects, non api answer is done

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -20,6 +20,8 @@ class ProjectsTest extends TestCase
         $this->post('/projects', $attributes);
 
         $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
     }
 
     /**
